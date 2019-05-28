39b17770b8c37160e16c8793224280dffd162091
qianyue <1974765766@qq.com>
2019-05-28 16:11:13 +0800

    添加一些demo

---
+++ for_set_shift_help.bat
@@ -0,0 +1,4 @@
+@echo off
+for /? > for.txt
+set /? > set.txt
+shift /? > shift.txt
--- helloworld.bat
+++ helloworld.bat
@@ -1,2 +1,12 @@
 echo hello world
+ping sz.tencent.com > a.txt 
+ping sz1.tencent.com >> a.txt 
+ping sz2.tencent.com >> a.txt 
+ping sz3.tencent.com >> a.txt 
+ping sz4.tencent.com >> a.txt 
+ping sz5.tencent.com >> a.txt 
+ping sz6.tencent.com >> a.txt 
+ping sz7.tencent.com >> a.txt 
+exit
 pause
+
